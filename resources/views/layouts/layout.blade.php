1af5eef5e2b6c49374b91d53e14d7aaffb9fd8e4
g <38024189+gggia36@users.noreply.github.com>
2021-03-02 15:44:50 +0700

    update home

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,9 +3,20 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="description" content="Bootstrap modal sidebar, hide on scroll navbar, Bootstrap 5 icons" />
+	<meta name="keywords" content="modal sidebar drawer, hide on scroll navbar, bootstrap 5 icons" /> -->
 	<title>flyncorp</title>
-	<!-- Bootstrap 5 -->
-	<link href="{{asset('assets/bootstrap5/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link rel="shortcut icon" href="{{asset('assets/image/logo/logo_footer.svg')}}">
+
+	
+	<link href="{{asset('assets/bootstrap4/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{asset('assets/fonts/stylesheet.css') }}" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
+	<link href="{{asset('assets/css/custom.css') }}" rel="stylesheet">
+	<link href="{{asset('assets/css/responsive.css') }}" rel="stylesheet">
+
 </head>
 <body>
 
@@ -18,7 +29,9 @@
 @include('layouts.footer')
 
 
-<script src="{{asset('assets/bootstrap5/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{asset('assets/bootstrap4/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{asset('assets/js/navbar.js') }}"></script>
 
 </body>
 </html>
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,6 +1,89 @@
 @extends('layouts.layout') @section('content')
 
-<h2>hi</h2>
+<section>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active position-relative">
+                <img src="{{asset('assets/image/product/banner_main.jpg')}}" class="d-block w-100" alt="banner flyncorp" />
+                <div class="center-img">
+                    <h1 class="text-uppercase fix-font">Professional Stairway</h1>
+                    <p>จำหน่ายไม้บันไดยางพาราอัดประสาน พร้อมบริการทำสีจากโรงงาน</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="d-flex align-items-center" id="bg-section1">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-5">
+                <img class="w-100" src="{{asset('assets/image/product/aboutus_image.png')}}">
+            </div>
+
+            <div class="col-md-7">
+                <h2 class="text-uppercase fix-font">FLYN Corperation co.th</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="py-5 my-5">
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-md-6">
+                <table>
+                    <tr>
+                        <td>
+                           <h1 class="text-blue p-30">25<span class="text-green">M</span></h1>
+                           <!-- <p class="text-uppercase">Successful</p> 
+                           <p class="text-uppercase">projects</p> -->
+                        </td>
+                        <td>
+                            <img class="h-122" src="{{asset('assets/image/product/Rectangle-5.jpg')}}">
+                        </td>
+                        <td>
+                            <p class="f-14 p-30 text-blue">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing.</p>
+                        </td>
+                    </tr>
+                </table>
+                
+            </div>
+            <div class="col-md-6">
+                 <table>
+                    <tr>
+                        <td>
+                           <h1 class="text-blue p-30">5</h1> 
+                          <!--  <p class="text-uppercase">Years in </p> 
+                           <p class="text-uppercase">business</p> -->
+                        </td>
+                        <td>
+                            <img class="h-122" src="{{asset('assets/image/product/Rectangle-5.jpg')}}">
+                        </td>
+                        <td>
+                            <p class="f-14 p-30 text-blue">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing.</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="row my-5">
+            <div class="col-md-12">
+                <img class="w-100" src="{{asset('assets/image/logo/testimonial_logo.png')}}">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        
+    </div>
+    
+</section>
+
 
 
 @endsection
