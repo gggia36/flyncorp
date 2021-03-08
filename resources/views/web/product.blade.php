8c88bf07fda6b90563ed6c4514cc8c88be96ab0a
g <38024189+gggia36@users.noreply.github.com>
2021-03-08 18:02:52 +0700

    update controller

---
+++ resources/views/web/product.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout') @section('content')
+<section class="" id="bg-section1">
+    <div class="container mt-5 pt-5">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h1 class="text-uppercase fix-font">PRODUCTS</h1>
+                <p>รายการไม้บันไดยางพาราประสานสำเร็จรูป (ขนาดมาตรฐานที่ใช้ทั่วไป)</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+@endsection
